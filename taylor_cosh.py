3a848687140cc8b7ea9d448c91c5fb6e2ebdfb30
lnsomniaX <fedor.gerasimov.032@gmail.com>
2021-10-15 22:55:11 +0300

    Taylor series.

--- taylor_cosh.py
+++ taylor_cosh.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import cmath
+import matplotlib.pyplot as plt
 
 ITERATIONS = 5
 
@@ -13,18 +15,15 @@ def my_cosh(x):
     multiplier = 1
     partial_sum = 1
     for n in range(1, ITERATIONS):
-        x_pow *= x**2  # В цикле постепенно считаем степень
-        multiplier *= 1/(2*n-1)/(2*n)  # (-1)^n и факториал
+        x_pow *= x ** 2  # В цикле постепенно считаем степень
+        multiplier *= 1 / (2 * n - 1) / (2 * n)  # (-1)^n и факториал
         partial_sum += x_pow * multiplier
 
     return partial_sum
 
-import matplotlib.pyplot as plt
 
 help(math.cosh)
 help(my_cosh)
-
-import cmath
 
 complex_arg = cmath.acosh(5)
 
@@ -37,27 +36,26 @@ y = my_cosh(p)
 
 plt.subplot(1, 2, 1)
 plt.grid()
-plt.plot(p, y,label='my_cosh')
-plt.plot(p, np.cosh(p), label='np.cosh',linestyle='dotted')
+plt.plot(p, y, label='my_cosh')
+plt.plot(p, np.cosh(p), label='np.cosh', linestyle='dotted')
 plt.legend(fontsize=8,
-               ncol=2,
-               facecolor='#f8f8ff',
-               edgecolor='black',
-               title_fontsize='8')
+           ncol=2,
+           facecolor='#f8f8ff',
+           edgecolor='black',
+           title_fontsize='8')
 plt.title('cosh при -2≤x≤2')
 
 p = np.r_[-10:10:0.01]
 y = my_cosh(p)
 
-plt.subplot (1, 2, 2)
+plt.subplot(1, 2, 2)
 plt.plot(p, y, label='my_cosh')
-plt.plot(p, np.cosh(p), label='np.cosh',linestyle = 'dotted')
+plt.plot(p, np.cosh(p), label='np.cosh', linestyle='dotted')
 plt.grid()
 plt.legend(fontsize=8,
-               ncol=2,
-               facecolor='#f8f8ff',
-               edgecolor='black',
-               title_fontsize='8')
+           ncol=2,
+           facecolor='#f8f8ff',
+           edgecolor='black',
+           title_fontsize='8')
 plt.title('cosh при -10≤x≤10')
 plt.show()
-
